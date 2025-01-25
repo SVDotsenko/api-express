d20f7895840c46b72f4f641b099069d1a45df413
Serhii Dotsenko <serdotsenko@gmail.com>
2025-01-25 15:00:05 +0000

    log format updated

--- app.js
+++ app.js
@@ -2,31 +2,37 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const winston = require('winston');
+const { format } = require('date-fns');
 const app = express();
 const port = 3001;
 
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+            const formattedTimestamp = format(new Date(timestamp), 'yyyy-MM-dd HH:mm:ss');
+            return `${formattedTimestamp} ${level}: ${message}`;
+        })
+    ),
     transports: [
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' })
+        new winston.transports.File({ filename: 'log.txt' })
     ]
 });
 
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-    const { fromCurrency, toCurrency, amount } = req.body;
+    const {fromCurrency, toCurrency, amount} = req.body;
     res.on('finish', () => {
-        const { rate, convertedAmount } = res.locals;
-        logger.info(`${req.method} ${req.url} - fromCurrency: ${fromCurrency}, toCurrency: ${toCurrency}, amount: ${amount}, rate: ${rate}, convertedAmount: ${convertedAmount}`);
+        const {rate, convertedAmount} = res.locals;
+        logger.info(`${amount} ${fromCurrency}, rate: ${rate}, ${convertedAmount} ${toCurrency}`);
     });
     next();
 });
 
 app.post('/convert', async (req, res) => {
-    const { fromCurrency, toCurrency, amount } = req.body;
+    const {fromCurrency, toCurrency, amount} = req.body;
 
     if (!fromCurrency || !toCurrency || !amount) {
         logger.error('Missing required parameters');
